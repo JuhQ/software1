e1d3eea32ef66a70c0bc1019ef934efd868846d2
Juha Tauriainen <juha@bin.fi>
2026-08-20 10:58:58 +0300

    Add solutions for mod3, ex2 and ex3

---
+++ module3/exercise2.py
@@ -0,0 +1,17 @@
+import math
+
+radius = input("Enter the radius of the circle: ")
+
+# radius = float(radius)
+
+area = float(radius) ** 2 * math.pi
+
+print("The area of the circle is " + str(area))
+
+print(area)
+
+print(radius * 5)
+
+print(float(radius) * 5)
+
+print(int(radius) * 5)
---
+++ numbers.py
@@ -0,0 +1,24 @@
+first = -9
+second = 12_456_123_180
+second1 = 12456123180
+third = 4.973
+fourth = -4 + 2j
+
+print(first)
+print(second)
+print(second1)
+print(third)
+print(fourth)
+print(fourth.real)
+print(fourth.imag)
+
+print()
+print()
+
+
+third = 4.973
+print(third + 1)
+
+third1 = 4.973
+print(third1 + 1)
+
