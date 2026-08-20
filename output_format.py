4f36a8607b607745be634c5a768b8b73b75b7e9a
Juha Tauriainen <juha@bin.fi>
2026-08-20 11:20:48 +0300

    Add output format examples

---
+++ output_format.py
@@ -0,0 +1,13 @@
+lunch_time = 1145
+
+print("My lunch time today will be " + str(lunch_time))
+print(f"My lunch time today will be {lunch_time}")
+print(f"My lunch time today will be {lunch_time:6.2f}")
+print(f"My lunch time today will be {lunch_time:6.4f}")
+print(f"My lunch time today will be {lunch_time:10.4f}")
+print(f"My lunch time today will be {lunch_time:15.4f}")
+
+
+# this will break python
+#print("My lunch time today will be " + lunch_time)
+
