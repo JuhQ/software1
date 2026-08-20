0bbdfd64b3b4a1fe817381d77df08311034529fb
Juha Tauriainen <juha@bin.fi>
2026-08-20 10:13:45 +0300

    Add mod3 exercise 1 solution

---
+++ myfile.py
@@ -0,0 +1,49 @@
+print("Hello")
+
+print('"Hello", said Juha')
+
+print("I'm Juha")
+print('"I\'m Juha"')
+
+print(10)
+print(5)
+
+
+print("Good")
+print("morning")
+
+
+print("Good\nmorning")
+
+
+
+print("Good \n morning")
+
+
+
+
+name = input("Name: ")
+print("You're name is " + name + "!")
+
+#print(name)
+#print("name")
+
+
+# this will print a text with name variable content in the end
+print("You're name is "+name)
+
+#print("You're name is name")
+
+
+
+
+points = 50  # points is now 50
+#print(points)  # prints: 50
+
+points = 120  # now points is 120
+#print(points)  # prints: 120
+
+
+player_name = "Juha"
+
+player_name = "Juha"
